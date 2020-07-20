f609a82b9c2a30ce5fdd44405848d76b5898b668
CalcoBoii <rosca.serban2@gmail.com>
2020-07-20 12:54:13 +0300

    Updates

---
+++ precache-manifest.e50e973df3d614efd27bcb63ee979f51.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f9febe73e0e17540ff0d0a9fe2795e",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "4af5b2d411d24ae29d99",
+    "url": "/WeatherApp/static/css/main.6384c0e0.chunk.css"
+  },
+  {
+    "revision": "7cf2182656a62ed6117a",
+    "url": "/WeatherApp/static/js/2.d437b892.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp/static/js/2.d437b892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4af5b2d411d24ae29d99",
+    "url": "/WeatherApp/static/js/main.46e0e009.chunk.js"
+  },
+  {
+    "revision": "1d501c6cbc4900159aea",
+    "url": "/WeatherApp/static/js/runtime-main.46a7f6c7.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/WeatherApp/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/WeatherApp/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
